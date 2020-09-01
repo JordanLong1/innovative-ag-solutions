4b76da5de3b42433379628cf33d82ea731c2e068
Jordan <jordanleelong5@gmail.com>
2020-08-31 19:06:58 -0700

    added basic actions, ready to test if working

--- innovative-ag-backend/app/controllers/users_controller.rb
+++ innovative-ag-backend/app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController 
     
+
+    # add before action ?
+    def show 
+
+        user = User.find_by(id: params[:id])
+        render json: user
+    end
+
+    def new
+        user = User.new(user_params)
+    end
+
+    def create 
+        user = User.new(user_params)
+         if user 
+            user.save 
+            session[:user_id] = user.id 
+            render json: user 
+         else 
+            render json: {error: "There is some incorrect info regarding your user information, please try again"}
+         end
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :first_name, :last_name, :email, :password_digest, :type, :bio)
+    end
 end
