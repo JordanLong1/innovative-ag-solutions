54e7f157b2857280cdfd585db6a66b4d1e383ffd
Jordan <jordanleelong5@gmail.com>
2020-08-31 19:06:17 -0700

    added sessions controller, phase one to test if its working correctly

--- innovative-ag-backend/app/controllers/sessions_controller.rb
+++ innovative-ag-backend/app/controllers/sessions_controller.rb
@@ -1,3 +1,18 @@
 class SessionsController < ApplicationController 
     
+    def create 
+        user = User.find_by(email: params[:email])
+
+        if user && user.authenticate(params[:password], params[:username])
+            session[:user_id] = user.id 
+            render json: user 
+        else 
+            render json: {error: "Username or password is incorrect", status: 404}
+        end
+    end
+
+    def destroy 
+        session.delete: user_id 
+
+    end
 end
