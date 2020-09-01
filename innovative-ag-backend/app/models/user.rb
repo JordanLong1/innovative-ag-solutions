f58809ee1070b6e33d856ce7b5807166282eb5eb
Jordan <jordanleelong5@gmail.com>
2020-08-31 17:27:25 -0700

    figured out bcrypt error problem, put has_secure_password macro back in

--- innovative-ag-backend/app/models/user.rb
+++ innovative-ag-backend/app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-    has_secure_password 
+    
 
     has_many :messages
+    has_secure_password
 
     def as_json(options={})
     super(options.merge({:methods => :type}))
   end
-  
+
 end
