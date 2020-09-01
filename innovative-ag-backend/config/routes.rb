37367b948b290d7af739d0698e870af3874cf48b
Jordan <jordanleelong5@gmail.com>
2020-08-31 17:26:42 -0700

    first phase of routes defined

--- innovative-ag-backend/config/routes.rb
+++ innovative-ag-backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create, :destroy]
+  resources :users, only: [:create, :show, :destroy]
+  resources :messages, only: [:create, :show, :index]
+  resources :crops_info, only: [:create, :show, :index, :edit, :update]
 end
