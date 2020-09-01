60f023e0a8d484c82c4e73518443ddf8394b0a44
Jordan <jordanleelong5@gmail.com>
2020-08-31 17:41:53 -0700

    added first phase of actions to test if things are working ok

--- innovative-ag-backend/app/controllers/messages_controller.rb
+++ innovative-ag-backend/app/controllers/messages_controller.rb
@@ -1,3 +1,33 @@
 class MessgesController < ApplicationController 
 
+
+    def index 
+        messages = Message.all 
+        render json: messages
+    end
+
+    def show 
+        message = Message.find_by(id: params[:id])
+        render json: message 
+    end
+
+    def new 
+        message = Message.new(message_params)
+    end
+
+    def create 
+        message = Message.new(message_params)
+        if message
+            message.save 
+            render json: message 
+        else 
+            render json: {error: "Invalid data please try again"}
+        end
+    end
+    
+    private 
+
+    def message_params 
+        params.require(:messages).permit(:content, :seen)
+    end
 end
