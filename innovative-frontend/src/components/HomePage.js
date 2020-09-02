661611ec20d04f47944b1ed09a2f00d21547a6fa
Jordan <jordanleelong5@gmail.com>
2020-09-02 16:09:21 -0700

    imported login, added comment to add onclick for the buttons

--- innovative-frontend/src/components/HomePage.js
+++ innovative-frontend/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SignUp from './SignUp'
+import Login from './Login'
 const HomePage = () => {
     return (
 
@@ -8,13 +9,21 @@ const HomePage = () => {
            <h1 className="ui center aligned header">Welcome to Innovative Ag Solutions!</h1>
         </div>
 
-       {/* <div className="ui container" style={{width: "500px", marginTop: "150px"}}>
-       <label> If you already have an account, click me</label>
-            <button className="massive ui button">Login!</button>
-       </div> */}
-       <div>
+        {/* {need to add on clicks that render those components} */}
+      <div>
+          <p>If you already have an account, click me</p>
+          <label>Login</label>
+          <button>Login</button> 
+      </div>
+
+      <div>
+          <p>If you're new here, click here to sign up</p>
+          <label>Sign up</label>
+          <button>Sign up</button>
+      </div>
+       {/* <div>
            <SignUp />
-       </div>
+       </div> */}
         </div>
 
 
