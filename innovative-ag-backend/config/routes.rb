2f3df0c8110c8aa7837b97a9ef4bdeba4324cd64
Jordan <jordanleelong5@gmail.com>
2020-09-01 18:46:51 -0700

    fixing routes

--- innovative-ag-backend/config/routes.rb
+++ innovative-ag-backend/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :show, :destroy]
   resources :messages, only: [:create, :show, :index]
   resources :crops_info, only: [:create, :show, :index, :edit, :update]
+  resources :pcas, only: [:create, :show, :destroy]
+  resources :growers, only: [:create, :show, :destroy]
 end
