bd3c57626f4d533814db1d8e7f4e3a6b7ca20a66
Jordan <jordanleelong5@gmail.com>
2020-09-01 18:11:40 -0700

    added .build into create actions

---
+++ innovative-ag-backend/app/controllers/growers_controller.rb
@@ -0,0 +1,28 @@
+class GrowersController < UserController 
+
+    def show 
+        grower = Grower.find_by(id: params[:user_id])
+    end
+
+    def new 
+        grower = Grower.new(grower_params)
+    end
+
+    def create 
+        pca = Pca.find_by(id: params[:id])
+        new_grower = Grower.new(grower_params)
+        grower = pca.new_grower.build(grower_params)
+        if grower 
+            grower.save 
+            render json: grower
+        else 
+            render json: {error: "Invalid data, please try again"}
+        end
+    end
+
+    private 
+
+    def grower_params 
+        params.require(:grower).permit(:username, :first_name, :last_name, :email, :password_digest, :type, :bio, :pca_id)
+    end
+end
