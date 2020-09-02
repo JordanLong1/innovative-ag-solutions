f0d2d4c8980a7c83d6cbfe1481ec91fddeeee778
Jordan <jordanleelong5@gmail.com>
2020-09-01 18:48:36 -0700

    basic skeleton for frontend

---
+++ innovative-frontend/src/components/App.js
@@ -0,0 +1,15 @@
+// bread & butter i think.. 
+
+import React, { component } from 'react';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                App component
+            </div>
+        )
+    };
+};
+
+export default App;
