d34a21146a168424ef726d6f094f755ff255e9dd
Jordan <jordanleelong5@gmail.com>
2020-09-01 18:05:17 -0700

    added id info to user params

--- innovative-ag-backend/app/controllers/users_controller.rb
+++ innovative-ag-backend/app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
     def create 
         user = User.new(user_params)
-         if user 
+         if user && user.authenticate(password: params[:password_digest])
             user.save 
             session[:user_id] = user.id 
             render json: user 
