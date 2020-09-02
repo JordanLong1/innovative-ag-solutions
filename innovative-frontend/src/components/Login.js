b25b7c3201da18d89b28411138752a2d1fe55a3b
Jordan <jordanleelong5@gmail.com>
2020-09-02 16:07:46 -0700

    built very basic login form with no styling

--- innovative-frontend/src/components/Login.js
+++ innovative-frontend/src/components/Login.js
@@ -3,7 +3,12 @@ import React from 'react'
 class Login extends React.Component {
     render() {
         return (
-            <div>Login</div>
+            <div>
+                <form>
+                    <input type="text" placeholder="Enter Username">Username</input><br></br>
+                    <input type="text" placeholder="Enter your password">Password</input>
+                </form>
+            </div>
         )
     }
 }
