42da304dd804c0e6649cc37c1ec57688ab289684
Jordan <jordanleelong5@gmail.com>
2020-09-02 22:31:37 -0700

    added libraries

--- innovative-frontend/src/index.js
+++ innovative-frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'
 import App from './components/App'
-// import { Provider } from 'react-redux'
-// import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './components/reducers/rootreducers.js'
 
-// const store = createStore(reducer)
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-    // <Provider>
+    <Provider store={store}>
         <App />
-    // </Provider>
+     </Provider>
     , document.getElementById('root')
 )
