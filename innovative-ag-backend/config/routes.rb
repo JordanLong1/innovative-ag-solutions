bd53d9a4333e25be8fd53683d59a83839c837394
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:43:38 -0700

    added route for getting current user

--- innovative-ag-backend/config/routes.rb
+++ innovative-ag-backend/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create, :destroy]
+  get '/get_current_user', to: 'sessions#get_current_user'
   resources :users, only: [:create, :show, :destroy]
   resources :messages, only: [:create, :show, :index]
   resources :crops_info, only: [:create, :show, :index, :edit, :update]
-  resources :pcas, only: [:create, :show, :destroy]
+  resources :pcas, only: [:create, :show, :destroy, :index]
   resources :growers, only: [:create, :show, :destroy]
 end
