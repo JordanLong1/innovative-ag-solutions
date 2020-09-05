27e899cf4f317dfbaf246a33a047d15e54538adf
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:43:53 -0700

    added attributes

--- innovative-ag-backend/app/serializers/user.rb
+++ innovative-ag-backend/app/serializers/user.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
     has_many :messages 
 
-    attributes: :id, :username, :first_name, :last_name, :email, :type, :bio, :grower_id, :pca_id
+    attributes: :user_id, :username, :first_name, :last_name, :email, :type, :bio, :grower_id, :pca_id
 end
