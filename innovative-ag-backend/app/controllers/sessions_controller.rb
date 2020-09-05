0856b6017bee11c49dfade115c2eba7c3b57e3a0
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:40:11 -0700

    can now pass cookies back and forth front to back

--- innovative-ag-backend/app/controllers/sessions_controller.rb
+++ innovative-ag-backend/app/controllers/sessions_controller.rb
@@ -1,18 +1,30 @@
 class SessionsController < ApplicationController 
     
     def create 
-        user = User.find_by(email: params[:email])
-
-        if user && user.authenticate(params[:password], params[:username])
+        binding.pry
+        user = User.find_by(email: params[:user][:email])
+        binding.pry
+        if user && user.authenticate(params[:user][:password])
             session[:user_id] = user.id 
+            binding.pry
+            resp = {
+                user: user.user_serializer
+            }
             render json: user 
         else 
+           resp = {
+               error: "Error test change later"
+           }
             render json: {error: "Username or password is incorrect", status: 404}
         end
     end
 
     def destroy 
-        session.delete: user_id 
+       session.clear
+
+       render json: {
+           message: "Successfully cleared logout"         
+       }
 
     end
 end
