90b56f165f49d6efa24e23424a5879b7a3896fa4
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:40:27 -0700

    fixed import errors

--- innovative-frontend/src/index.js
+++ innovative-frontend/src/index.js
@@ -4,12 +4,12 @@ import App from './components/App'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import reducers from './components/reducers/rootreducers.js'
+import rootreducers from './components/reducers/rootreducers.js'
 
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(rootreducers, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <Provider store={store}>
