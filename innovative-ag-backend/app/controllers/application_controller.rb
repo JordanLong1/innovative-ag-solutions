3193ba287175a1c0d7516c5362c85636f8487334
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:39:28 -0700

    added helper methods for current user, logged in

--- innovative-ag-backend/app/controllers/application_controller.rb
+++ innovative-ag-backend/app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+
+       def current_user 
+        @current_user ||= User.find_by(id: session[:user_id])
+         #saves a hit to the database
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+
+    def get_current_user 
+        if logged_in? 
+            render json: {user: current_user.user_serializer}
+        else 
+            render json: {error: "There is no current user"}
+        end
+    end
 end
