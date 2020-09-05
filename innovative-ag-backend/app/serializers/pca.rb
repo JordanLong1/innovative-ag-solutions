6aaadb6b3afdf082640a05d76a0c588918af3e1c
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:42:58 -0700

    fixed attributes

--- innovative-ag-backend/app/serializers/pca.rb
+++ innovative-ag-backend/app/serializers/pca.rb
@@ -1,6 +1,6 @@
 class PcaSerializer < ActiveModel::Serializer
 
-    attributes: :id, :username, :first_name, :last_name, :email, :type, :bio, :user_id
+    attributes: :id, :username, :first_name, :last_name, :email, :type, :bio, :password
 
     has_many :growers 
 end
