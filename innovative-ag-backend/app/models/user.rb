a8ebbae985d87c8ca7b32e0ce44ca11a15a06ac7
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:43:08 -0700

    added method for serializer

--- innovative-ag-backend/app/models/user.rb
+++ innovative-ag-backend/app/models/user.rb
@@ -7,8 +7,26 @@ class User < ApplicationRecord
     # validates :username, :email, uniqueness: true
     # validates :username, :first_name, :last_name, :email, :password_digest, :type, :bio, presence: true
 
+
+    def user_serializer
+      {
+        username: username, 
+        email: email, 
+        first_name: first_name, 
+        last_name: last_name, 
+        type: type, 
+        bio: bio,
+        id: id, 
+        password: password
+
+      }
+
+    end
+
     def as_json(options={})
     super(options.merge({:methods => :type}))
   end
+
+
 
 end
