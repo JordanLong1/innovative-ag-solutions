fef7195790ec1ff6b7093e0cca9772583c6e706f
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:43:25 -0700

    took out authenticate and put in proper place

--- innovative-ag-backend/app/controllers/users_controller.rb
+++ innovative-ag-backend/app/controllers/users_controller.rb
@@ -13,10 +13,9 @@ class UsersController < ApplicationController
     end
 
     def create 
-        binding.pry
         user = User.new(user_params)
-         if user && user.authenticate(password: params[:password_digest])
-            user.save 
+         if user 
+            user.save!
             session[:user_id] = user.id 
             render json: user 
          else 
@@ -27,6 +26,6 @@ class UsersController < ApplicationController
     private 
 
     def user_params 
-        params.require(:user).permit(:username, :first_name, :last_name, :email, :password_digest, :type, :bio)
+        params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :type, :bio, :user_id, :pca_id, :grower_id)
     end
 end
