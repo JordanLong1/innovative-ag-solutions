302e5a449e109abaae8412d01fbdb00f7a883b97
Jordan <jordanleelong5@gmail.com>
2020-09-05 15:41:07 -0700

    added link tags

--- innovative-frontend/src/components/HomePage.js
+++ innovative-frontend/src/components/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import SignUp from './SignUp'
-import Login from './Login'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+import { Link } from 'react-router-dom'
 const HomePage = () => {
 
 
@@ -16,20 +15,15 @@ const HomePage = () => {
 
       <div>
           <p>If you already have an account, click me</p>
-          <BrowserRouter >
           <Link to='/login'>Login</Link> 
 
-          <Route path='/login' exact component={Login}> 
-          </Route>
-          </BrowserRouter>
       </div>
 
       <div>
           <p>If you're new here, click here to sign up</p>
-         <BrowserRouter>
+         
          <Link to='/signup'>Sign up</Link>
-         <Route path='/signup' exact component={SignUp}></Route>
-         </BrowserRouter>
+         
       </div>
      
         </div>
