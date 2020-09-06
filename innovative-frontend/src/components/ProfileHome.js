8969e7b5c12f61839d2682b4931d6cd4c5eaaa15
Jordan <jordanleelong5@gmail.com>
2020-09-05 18:06:50 -0700

    added profile home to be rendered after logging in

---
+++ innovative-frontend/src/components/ProfileHome.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import Navbar from './Navbar'
+class ProfileHome extends React.Component {
+    render() {
+        return (
+            <div>PCA or Growers homepage
+                <Navbar />
+            </div>
+        )
+    }
+}
+
+export default ProfileHome
