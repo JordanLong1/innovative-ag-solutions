6f341b2001faea4b2591f6ce6b99c555c8dd4a7c
Jordan <jordanleelong5@gmail.com>
2020-09-06 17:36:57 -0700

    added get_current_user method

--- innovative-ag-backend/app/controllers/sessions_controller.rb
+++ innovative-ag-backend/app/controllers/sessions_controller.rb
@@ -24,4 +24,13 @@ class SessionsController < ApplicationController
        }
 
     end
+
+    def get_current_user 
+        if logged_in? 
+            render json: {user: current_user.user_serializer}
+        else 
+            render json: {error: "There is no current user"}
+        end
+    end
+
 end
