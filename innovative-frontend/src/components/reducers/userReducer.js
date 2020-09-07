ac66b4bfb96cfcf7e6d94f84ee48661ccd8dd8e7
Jordan <jordanleelong5@gmail.com>
2020-09-06 17:38:28 -0700

    added CURRENT_USER for now. may separate later

--- innovative-frontend/src/components/reducers/userReducer.js
+++ innovative-frontend/src/components/reducers/userReducer.js
@@ -3,6 +3,8 @@ export default ( state = {}, action) => {
     switch(action.type) {
         case 'LOGIN_USER':
             return action.user
+            case 'CURRENT_USER': 
+            return action.payload.user
         default:
         return state
     }
