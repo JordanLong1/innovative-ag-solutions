97b929e868688336e9fb80cf396d8febcf02dcc7
Jordan <jordanleelong5@gmail.com>
2020-09-06 17:38:01 -0700

    getCurrentUser action creator is working properly

--- innovative-frontend/src/components/actions/index.js
+++ innovative-frontend/src/components/actions/index.js
@@ -32,7 +32,7 @@ export const createUser = (newUser) => {
     }
 }
 
-export const getPcas = (pca) => {
+export const getPcas = (pca) => { // need to fetch here to put all salesmen into a dropdown for a grower to sign up
     return(dispatch) => {
         return fetch('http://localhost:3000/pcas')
         .then(resp => resp.json()) 
@@ -42,12 +42,18 @@ export const getPcas = (pca) => {
 
 export const getCurrentUser = userInfo => {
     return(dispatch) => {
-        return fetch('http://localhost:3000/get_curent_user', {
+        return fetch('http://localhost:3000/get_current_user', {
             credentials: "include", 
             headers: {
                 'Content-Type': 'application/json'
             }
             
+        })
+        .then(resp => resp.json()) 
+        .then(resp => {
+            dispatch({type: 'CURRENT_USER', payload: resp})
+            // console.log('this is response', resp)
+            // console.log('user info', userInfo)
         })
     }
 }   
