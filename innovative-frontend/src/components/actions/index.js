cb6d867b4af6088c51cd64cd9c4c06c27b815604
Jordan <jordanleelong5@gmail.com>
2020-09-07 17:58:13 -0700

    added pcaid

--- innovative-frontend/src/components/actions/index.js
+++ innovative-frontend/src/components/actions/index.js
@@ -1,10 +1,11 @@
 
 
-export const createUser = (newUser) => {
+export const createUser = (newUser, callback) => {
   
     return (dispatch) => {
         return fetch("http://localhost:3000/users", {
             method: "POST", 
+            credentials: "include",
             headers: {
                 'Content-Type': 'application/json',
             }, 
@@ -18,25 +19,34 @@ export const createUser = (newUser) => {
                     email: newUser.email,
                     password: newUser.password, 
                     type: newUser.type, 
-                    bio: newUser.bio
+                    bio: newUser.bio, 
+                    pca_id: newUser.pcaId
                 }
             }
             ),
 
         })
         .then(resp => resp.json())
-        .then(resp => console.log('hello', resp))
+        .then(resp => console.log('create new user resp', resp))
+        .then(resp => {
+            dispatch({ type: 'LOGIN_USER', user: resp})
+            callback()
+        })
         //.then(resp => {
 
          //})   
     }
 }
 
-export const getPcas = (pca) => { // need to fetch here to put all salesmen into a dropdown for a grower to sign up
+export const getPcas = () => { // need to fetch here to put all salesmen into a dropdown for a grower to sign up
+    
     return(dispatch) => {
         return fetch('http://localhost:3000/pcas')
         .then(resp => resp.json()) 
-        .then(resp => console.log(resp))
+        .then(resp => {
+            // console.log(resp)
+            dispatch({ type: 'GET_PCAS', pcas: resp})
+        })
     }
 }
 
@@ -51,9 +61,11 @@ export const getCurrentUser = userInfo => { // add conditional for errors
         })
         .then(resp => resp.json()) 
         .then(resp => {
-            dispatch({type: 'CURRENT_USER', payload: resp})
-            // console.log('this is response', resp)
-            // console.log('user info', userInfo)
+            if(!resp.error) {
+                dispatch({type: 'CURRENT_USER', payload: resp})
+            } else {
+                alert(resp.error)
+            }
         })
     }
 }   
@@ -97,7 +109,6 @@ export const logOutUser = id => {
         })
         .then(resp => resp.json())
         .then(resp => {
-            console.log('this is the resp', resp)
             dispatch({ type: 'LOGOUT_USER', payload: resp })
         })
     }
