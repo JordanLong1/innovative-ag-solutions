7e195ffce821e88e195d89386f36908e05ad305a
Jordan <jordanleelong5@gmail.com>
2020-09-07 17:58:33 -0700

    added pcareducers

--- innovative-frontend/src/components/reducers/rootreducers.js
+++ innovative-frontend/src/components/reducers/rootreducers.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux'
 import userReducer from './userReducer'
+import pcaReducers from './pcasReducer'
 export default combineReducers({
-        userReducers: userReducer
+        userReducers: userReducer, 
+        pcaReducer: pcaReducers
     })
 
