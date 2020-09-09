e092b1046bc339ea7869e3bc22b103a768cb44eb
Jordan <jordanleelong5@gmail.com>
2020-09-08 21:41:42 -0700

    can conditionally render what a grower/pca sees upon login

--- innovative-frontend/src/components/Navbar.js
+++ innovative-frontend/src/components/Navbar.js
@@ -5,34 +5,63 @@ import {logOutUser} from './actions'
 
 class Navbar extends React.Component {
 
+  
 
   handleClick = () => {
     this.props.logOutUser()
   }
 
+   conditionallyRenderUserInfo = () => {
+      if (this.props.user.type === "Grower") {
+        return <div className="ui menu">
+   
+          <Link to='/addcrops' className='item'>Add Crops</Link>
+      
+
+    
+         
+          <Link to='/mycrops' className='item'>My Crops</Link>
+     
+         
+    
+          <Link to='/messages' className='item'>Messages</Link>
+         
+    
+          <Link onClick={this.handleClick} to='/' className='item'>Logout</Link>
+         </div>
+      } else {
+        return <div className="ui menu">
+        <Link to='/growerscrops' className='item'>Crops Attended to</Link>
+        
+        <Link to='/messages' className='item'>Messages</Link>
+        
+        <Link onClick={this.handleClick} to='/' className='item'>Logout</Link>
+        
+      </div>
+      }
+   }
+
     render () {
+     
+
         return (
+          
             <div className="ui menu">
             <div className="header item">
-             Home
+              {this.conditionallyRenderUserInfo()}
             </div>
-            <a className="item" href=''>
-              My Growers
-            </a>
-            <a className="item" href=''>
-              Messages
-            </a>
-            <a className="item" href=''>
-              Rec-Forms
-            </a>
-            <div className="right menu">
-              <div className="header item"> 
-                <Link onClick={this.handleClick} to='/' className='item'>Logout</Link>
-              </div>
-            </div>
+           
           </div>
         )
     }
 }
 
-export default connect(null, {logOutUser})(Navbar);
+// need acess to redux so you can find out what user is logged in mapStateToProps, pull user object in from store write  ahelper function
+// check and see if this.props.user.type === grower || pca 
+
+const mapStateToProps = state => {
+  // console.log(state.userReducers.user)
+  return {user: state.userReducers.user}
+}
+
+export default connect(mapStateToProps, {logOutUser})(Navbar);
