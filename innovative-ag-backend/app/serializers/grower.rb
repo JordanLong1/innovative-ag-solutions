595505dec24689b996ffed091783324c4b958c86
Jordan <jordanleelong5@gmail.com>
2020-09-08 21:46:11 -0700

    added has_many crop_infos

--- innovative-ag-backend/app/serializers/grower.rb
+++ innovative-ag-backend/app/serializers/grower.rb
@@ -1,6 +1,7 @@
 class GrowerSerializer < ActiveModel::Serializer 
 
     belongs_to :salesman
+    has_many :crop_infos
 
     attributes: :id, :username, :first_name, :last_name, :email, :type, :bio, :pca_id, :user_id
 
