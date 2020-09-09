06097c6e7ad9b0fa421b9e6d7cf3458cd474f0b5
Jordan <jordanleelong5@gmail.com>
2020-09-08 21:44:47 -0700

    fixed typo and took out prs

--- innovative-ag-backend/app/controllers/users_controller.rb
+++ innovative-ag-backend/app/controllers/users_controller.rb
@@ -13,9 +13,12 @@ class UsersController < ApplicationController
     end
 
     def create 
+       binding.pry
         user = User.new(user_params)
+        binding.pry
          if user 
             user.save!
+            binding.pry
             render json: user 
          else 
             render json: {error: "There is some incorrect info regarding your user information, please try again"}
