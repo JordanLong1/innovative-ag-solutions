2c1792e1e8c00c58ae9f166d56b030f83ffd4da0
Jordan <jordanleelong5@gmail.com>
2020-09-09 22:00:07 -0700

    added addcrops route

--- innovative-frontend/src/components/App.js
+++ innovative-frontend/src/components/App.js
@@ -7,6 +7,7 @@ import ProfileHome from './ProfileHome'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import {getCurrentUser} from './actions'
+import CropInfoForm from './CropInfoForm';
 class App extends React.Component {
 
     componentDidMount() {
@@ -22,7 +23,7 @@ class App extends React.Component {
                 <Route exact path='/login' component={Login} />
                 <Route exact path='/signup' component={SignUp} />
                 <Route exact path='/profilehome' component={ProfileHome} />
-           
+                <Route exact path='/addcrops' component={CropInfoForm} />
                 </BrowserRouter>
                 
             </div>
