a5b84158f76bb47132811aebdc855b78a48d10cc
Jordan <jordanleelong5@gmail.com>
2020-09-10 20:09:41 -0700

    added cropsreducer to root

--- innovative-frontend/src/components/reducers/rootreducers.js
+++ innovative-frontend/src/components/reducers/rootreducers.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
 import userReducer from './userReducer'
 import pcaReducers from './pcasReducer'
+import cropsReducer from './cropsReducer'
 export default combineReducers({
         userReducers: userReducer, 
-        pcaReducer: pcaReducers
+        pcaReducer: pcaReducers, 
+        cropsReducer: cropsReducer
     })
 
