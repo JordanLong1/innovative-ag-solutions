a56f27d236eda2cc8904ee2d17f5a0d53c526712
Jordan <jordanleelong5@gmail.com>
2020-09-10 23:23:24 -0700

    added rescue

--- innovative-ag-backend/app/controllers/application_controller.rb
+++ innovative-ag-backend/app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::API
 
        def current_user 
+       begin 
         current_user ||= User.find_by(id: session[:user_id])
+
+       rescue 
+        return nil
+       end
+       end
          #saves a hit to the database
-    end
+
 
     def logged_in?
         !!current_user
