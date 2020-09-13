b136c459c71ae598302d4211fcbcb4ab688cbeaf
Jordan <jordanleelong5@gmail.com>
2020-09-12 18:39:28 -0700

    made initial state array instead of object

--- innovative-frontend/src/components/reducers/cropsReducer.js
+++ innovative-frontend/src/components/reducers/cropsReducer.js
@@ -1,8 +1,8 @@
-export default (state = {}, action) => {
+export default (state = [], action) => {
     switch(action.type) {
         case 'ADD_CROP':
            
-            return {...state, crop: action.crop}
+            return [...state, action.crop]
             default:
                 return state
     }
