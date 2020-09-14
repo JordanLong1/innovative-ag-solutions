c9e9382cdacf86e94ffac12af3362efa78d88294
Jordan <jordanleelong5@gmail.com>
2020-09-13 23:51:02 -0700

    phase one of styling the homepage

--- innovative-frontend/src/components/HomePage.js
+++ innovative-frontend/src/components/HomePage.js
@@ -13,16 +13,16 @@ const HomePage = () => {
            <h1 className="ui center aligned header">Welcome to Innovative Ag Solutions!</h1>
         </div>
 
-      <div>
-          <p>If you already have an account, click me</p>
-          <Link to='/login'>Login</Link> 
+      <div className='ui container' style={{marginTop: '100px'}}>
+          <p style={{ fontSize: '50px'}}>Already have an account?</p>
+          <Link to='/login' style={{ fontSize: '50px'}}>Login</Link> 
 
       </div>
 
-      <div>
-          <p>If you're new here, click here to sign up</p>
+      <div className='ui container' style={{marginTop: '110px'}}>
+          <p style={{ fontSize: '50px'}}>New here huh? Let's get you signed up</p>
          
-         <Link to='/signup'>Sign up</Link>
+         <Link to='/signup' style={{ fontSize: '50px'}}>Sign up</Link>
          
       </div>
      
