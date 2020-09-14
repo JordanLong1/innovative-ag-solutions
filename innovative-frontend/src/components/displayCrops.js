8fa9b438e8733f03c3ed6ea99a72923622e4e393
Jordan <jordanleelong5@gmail.com>
2020-09-13 20:46:58 -0700

    all of growers crops are now being shown successfully

--- innovative-frontend/src/components/displayCrops.js
+++ innovative-frontend/src/components/displayCrops.js
@@ -1,25 +1,39 @@
 import React from 'react' 
 import { connect } from 'react-redux'
-// import {createCropInfo} from './actions'
+
+
+
+// const handleGrowersCrops = (props) => {}
+
+
  const displayCrops = (props) => {
-     console.log(props)
+    const cropList = props.crops.map(crop => {
+        return (
+            <div className='each crop' key={crop.id} >
+                <li >Name - {crop.name}</li>
+                <li>Description - {crop.description}</li>
+                <li>Acres - {crop.amount_of_acres}</li>
+            </div>
+        )
+     })
     return (
-        <div>
+        
             <div>
                 <h1>Your Crops</h1>
-                    <p>Name of crop - {props.crop.name}</p>
-                    <p>Description of crop - {props.crop.description}</p>
-                    <p>Acres - {props.crop.amount_of_acres}</p>
+                    <ul>
+                    {cropList}
+                    </ul>
             </div>
-           
-        </div>
+       
     )
 }
 
 const mapStateToProps = state => {
-   console.log(state.cropsReducer)
+//    console.log(state.userReducers.id) gets us user id
+        // console.log(state.cropsReducer.filter(crops => console.log(crop.grower_id === state.userReducers.id))) THIS GIVES US WHAT WE WANT I THINK
    // filter by the current user id (grower id)
-    return {crop: state.cropsReducer.crop} //.filter(crop => crop.grower_id === user.id) || state.userReducer.id
+    return {crops: state.cropsReducer.filter(crop => crop.grower_id === state.userReducers.id)} //.filter(crop => crop.grower_id === user.id) || state.userReducer.id
+    // .crop.filter(crop => crop.id )
  
 }
 
