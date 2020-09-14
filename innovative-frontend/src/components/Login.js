4a6c35de31650025f6a49d110b325ddd42345ced
Jordan <jordanleelong5@gmail.com>
2020-09-14 11:53:05 -0700

    moved welcome header to login page

--- innovative-frontend/src/components/Login.js
+++ innovative-frontend/src/components/Login.js
@@ -26,23 +26,31 @@ class Login extends React.Component {
     
     render() {
         return (
+            <div>
+
+        
+            <div className="ui segment">
+            <h1 className="ui center aligned header">Welcome to Innovative Ag Solutions!</h1>
+            </div>
             <div className='ui container' style={{width: '500px', marginTop: '150px'}}>
+              
                 <h1>Log-In below</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className='ui input'>
-                    <label>Email</label>
+                    <label className='ui pointing label'>Email</label>
                     <input style={{width: '300px'}} type="text" name="email" placeholder="Enter email" onChange={this.handleChange} value={this.state.email}/> <br></br>
                     </div> <br></br>
                     <div className='ui input'>
 
-                    <label>Password</label>
+                    <label className='ui pointing label'>Password</label>
                     <input style={{width: '300px'}} type="password" name="password" placeholder="Enter your password" onChange={this.handleChange} value={this.state.password}/><br></br>
-                    </div> <br></br>
+                    </div> <br></br><br></br>
                     
                     
                     <button className='ui large button' type="submit" >Submit</button>
                     
                 </form>
+                </div>
             </div>
         )
     }
