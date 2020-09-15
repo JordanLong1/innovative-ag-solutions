5a47a38ba6cdb5d40f516077875d8b29db791c36
Jordan <jordanleelong5@gmail.com>
2020-09-14 23:22:57 -0700

    took out unneccesary user_id

--- innovative-ag-backend/app/serializers/grower.rb
+++ innovative-ag-backend/app/serializers/grower.rb
@@ -3,6 +3,6 @@ class GrowerSerializer < ActiveModel::Serializer
     belongs_to :salesman
     has_many :crop_infos
 
-    attributes: :id, :username, :first_name, :last_name, :email, :type, :bio, :pca_id, :user_id
+    attributes: :id, :username, :first_name, :last_name, :email, :type, :bio, :pca_id
 
 end
