bf9f17999df8b538aed3ef8cc934f6351cf17827
Jordan <jordanleelong5@gmail.com>
2020-09-15 16:13:00 -0700

    added GET CROPS

--- innovative-frontend/src/components/reducers/cropsReducer.js
+++ innovative-frontend/src/components/reducers/cropsReducer.js
@@ -1,8 +1,10 @@
 export default (state = [], action) => {
     switch(action.type) {
-        case 'ADD_CROP':
-           
+        case 'ADD_CROP':  
             return [...state, action.crop]
+            case 'GET_CROPS':
+                // console.log(action)
+                return action.crops
             default:
                 return state
     }
