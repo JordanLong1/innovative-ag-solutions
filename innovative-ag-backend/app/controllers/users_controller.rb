68c39faf5843abde11f2ff8a919092a5c23f4827
Jordan <jordanleelong5@gmail.com>
2020-09-14 23:24:34 -0700

    took out unneccessary user_id

--- innovative-ag-backend/app/controllers/users_controller.rb
+++ innovative-ag-backend/app/controllers/users_controller.rb
@@ -28,6 +28,6 @@ class UsersController < ApplicationController
     private 
 
     def user_params 
-        params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :type, :bio, :user_id, :pca_id, :grower_id)
+        params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :type, :bio, :pca_id, :grower_id)
     end
 end
