c092c0f16c06c22fe859a2961fded9b90802b0c6
Jordan <jordanleelong5@gmail.com>
2020-09-15 12:05:20 -0700

    added grower_id

--- innovative-ag-backend/app/serializers/pca.rb
+++ innovative-ag-backend/app/serializers/pca.rb
@@ -1,6 +1,6 @@
 class PcaSerializer < ActiveModel::Serializer
 
-    attributes: :id, :username, :first_name, :last_name, :email, :type, :bio, :password
+    attributes: :id, :username, :first_name, :last_name, :email, :type, :bio, :password, :grower_id
 
     has_many :growers 
 end
