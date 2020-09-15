cbd1712001ff67e8697a7ed6eca107c2b2fa9ef4
Jordan <jordanleelong5@gmail.com>
2020-09-14 23:22:21 -0700

    added componentDidMount to fetch salesmans show

--- innovative-frontend/src/components/ProfileHome.js
+++ innovative-frontend/src/components/ProfileHome.js
@@ -1,21 +1,27 @@
 import React from 'react' 
 import Navbar from './Navbar'
 import { connect } from 'react-redux'
+import {getPcaToAccessGrowersCrops} from './actions'
 
 class ProfileHome extends React.Component {
 
+    componentDidMount() {
+        console.log('component did mount', this.props.user)
+        if (this.props.user.type === 'Pca') {
+            this.props.getPcaToAccessGrowersCrops(this.props.user.id)
+        }
+    }
 
 
     render() {
         return (
             <div className='navbar importer'>
                 <Navbar />
-            <div style={{marginTop: '150px', textAlign: 'center'}}>
+            <div className='ui container' style={{marginTop: '150px', textAlign: 'center'}}>
                 <div className='welcome user'>
                 <h4>Welcome {this.props.user.username}</h4><br></br>
                 About me
                 <ul> 
-                    {/* <li>Name: {`${this.props.user.firstName} ${this.props.user.lastName}`}</li> */}
                     <li>Name: {`${this.props.user.first_name} ${this.props.user.last_name}`}</li>
                      <li>Email: {this.props.user.email}</li>
                      <li>Bio: {this.props.user.bio}</li>
@@ -36,4 +42,4 @@ const mapStateToProps = state => {
     return {user: state.userReducers}
 }
 
-export default connect(mapStateToProps)(ProfileHome)
+export default connect(mapStateToProps, {getPcaToAccessGrowersCrops})(ProfileHome)
