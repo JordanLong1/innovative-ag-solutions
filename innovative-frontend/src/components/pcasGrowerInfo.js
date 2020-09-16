7e5057f6d3e994508750e7d09e5556aeac2f94fe
Jordan <jordanleelong5@gmail.com>
2020-09-15 23:34:39 -0700

    headers displaying above lis

--- innovative-frontend/src/components/pcasGrowerInfo.js
+++ innovative-frontend/src/components/pcasGrowerInfo.js
@@ -3,33 +3,34 @@ import { connect } from 'react-redux'
 import Navbar from './Navbar'
 
 
+
+
  const pcasGrowerInfo = (props) => {
      const listCropsAndGrowers = props.growersCrops.map(eachArr => {
         return eachArr.map(objects => {
-            // return console.log(objects)
-                return (
-                    <div className='ui container' key={objects.id}> 
-                    <div >
-                    {'username' in objects ? <h3 >Grower Accounts - {objects.username}</h3> : null}
-                    </div>
-                    <ul>
-               {'name' in objects ? <li>Name of crops {objects.name}</li> : null } 
-                {'description' in objects ?  <li>Crop Description{objects.description}</li> : null }
-                {'amount_of_acres' in objects ? <li>Acres accounted for - {objects.amount_of_acres}</li> : null } 
 
-                    </ul>
-                    </div>
+                return (
+                    <div key={objects.id}> <br></br>
                    
-                )
-                    
-                    
+                    {'username' in objects ? <h3 style={{position: 'relative', bottom: '175px'}}>Grower Accounts - {objects.username}</h3> : null}
+               {'name' in objects ? <li style={{position: 'relative', top: '200px'}}>Name of crops - {objects.name}</li> : null } 
+                {'description' in objects ?  <li style={{position: 'relative', top: '200px'}}>Crop Description - {objects.description}</li> : null }
+                {'amount_of_acres' in objects ? <li style={{position: 'relative', top: '200px'}}>Acres accounted for - {objects.amount_of_acres}</li> : null } 
+
+                    </div>  
+                )      
             })
         })
 
     return (
-        <div>
+        <div className='test div'>
             <Navbar />
+            <div className='ui container'>
+
+            <h1 style={{textAlign: 'center'}}>Crops currently attended to:</h1>
             {listCropsAndGrowers}
+          
+            </div>
         </div>
     )
 }
