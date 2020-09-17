6051038b2a39f90f546e245865056dfcde585f9f
Jordan <jordanleelong5@gmail.com>
2020-09-17 10:52:28 -0700

    extracted header

---
+++ innovative-frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+const Header = () => {
+    // console.log(props)
+    return (
+        
+        <div className="ui segment">
+        <h1 className="ui center aligned header">Welcome to Innovative Ag Solutions!</h1>
+        </div>
+    )
+}
+
+export default Header
