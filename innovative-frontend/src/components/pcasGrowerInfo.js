8d2755cee2b0e6c6016b8436f65acbb8058bcf61
Jordan <jordanleelong5@gmail.com>
2020-09-23 15:28:41 -0700

    finished refactor. separated cropList function & pcasGrowerInfo

--- innovative-frontend/src/components/pcasGrowerInfo.js
+++ innovative-frontend/src/components/pcasGrowerInfo.js
@@ -2,28 +2,29 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Navbar from './Navbar'
 
+const cropList = (cropsArr) => {
+   return cropsArr.map(crop => {
+      return (
+     <li>
+        Name of Crop - {crop.name}<br></br>
+         Description - {crop.description}<br></br>
+         Acres - {crop.amount_of_acres}
+     </li>
+      )
+    })
+    } 
 
 const pcasGrowerInfo = (props) => {
     const listCropsAndGrowers = props.growersCrops.map(objects => {
-      console.log(objects)
         return (
             <div key={objects.id}>
                 <h3>Grower Account - {objects.grower_info.username}</h3>
-                {/* {cropInfoToList} */}
+                <ul key={objects.id}>
+                {cropList(objects.crops)}
+            
+                 </ul>
             </div>
         )
-    //    return eachArr.map(objects => {
-    //            return (
-    //                <div className='crop-info-container' key={objects.id}> <br></br>
-    //                    <div className='growers-names'>
-    //                      <h3>Grower Account - {objects.username}</h3><br></br>
-    //                    </div>
-    //                    <li>Name of crops - {objects.name}</li>
-    //                    <li>Crop Description - {objects.description}</li>
-    //                    <li>Acres accounted for - {objects.amount_of_acres}</li>
-    //                </div>  
-    //            )      
-    //        })
        })
    return (
        <div className='test div'>
@@ -42,7 +43,4 @@ const mapStateToProps = state => {
 export default connect(mapStateToProps)(pcasGrowerInfo)
 
   
-// {'username' in objects ? <h3 style={{position: 'relative', bottom: '175px'}}>Grower Accounts - {objects.username}</h3> : null}
-// {'name' in objects ? <li style={{position: 'relative', top: '315px'}}>Name of crops - {objects.name}</li> : null } 
-//  {'description' in objects ?  <li style={{position: 'relative', top: '315px'}}>Crop Description - {objects.description}</li> : null }
-//    {'amount_of_acres' in objects ? <li style={{position: 'relative', top: '315px'}}>Acres accounted for - {objects.amount_of_acres}</li> : null } 
+ 
