55cf058ec503742f53dfb72dadd831159b891d18
Jordan <jordanleelong5@gmail.com>
2020-09-23 00:17:27 -0700

    refactored and cleaned up response rendered

--- innovative-ag-backend/app/controllers/pcas_controller.rb
+++ innovative-ag-backend/app/controllers/pcas_controller.rb
@@ -6,24 +6,32 @@ class PcasController < UsersController
     end
 
     def show 
-        p = Pca.find_by(id: params[:id])
+        # p = Pca.find_by(id: params[:id])
        
-        if p.growers.count == 0 
+        # if p.growers.count == 0 
           
-            render json: {error: "will this work?"}
+        #     render json: {error: "You need your growers to sign up and load their crops."}
             
-        else  
-            grower_info = p.growers
-           crop = grower_info.map do |grower| 
-                 grower.crop_infos.map do |crop| 
-                    crop
-                end
-            end
-            pca = crop << grower_info
-            render json: pca
+        # else  
+        #     grower_info = p.growers
+        #    crop = grower_info.map do |grower| 
+        #          grower.crop_infos.map do |crop| 
+        #             crop
+        #         end
+        #     end
+        #     pca = crop << grower_info
+        #     render json: pca
+        # end
+        
+        p = Pca.find_by(id: params[:id])
+        if p.growers.count == 0 
+            render json: {error: "You need your groweto sign up and load their crops."}
+        else
+          pca_accounts_info = growers_and_crops(p)
+          render json: pca_accounts_info
         end
 
-        end
+    end
 
     def new 
         pca = Pca.new(pca_params)
@@ -40,6 +48,15 @@ class PcasController < UsersController
     end
 
     private 
+
+    def growers_and_crops(pca)
+        all_grower_and_crop_info = []
+        pca.growers.each do |grower|
+          grower = {grower_info: grower, crops: grower.crop_infos}
+          all_grower_and_crop_info << grower
+        end
+        all_grower_and_crop_info
+      end
 
     def pca_params 
         params.require(:pca).permit(:id, :username, :first_name, :last_name, :email, :password, :type, :bio, :grower_id, :pca_id)
